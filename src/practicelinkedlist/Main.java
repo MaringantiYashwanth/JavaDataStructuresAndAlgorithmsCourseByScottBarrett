f460a8fee125b56abb28332ef1772a2103fccb3a
MaringantiYashwanth <yashwanthmaringanti@gmail.com>
2024-05-14 04:33:45 +0530

    initial commit - practice linked list

---
+++ src/practicelinkedlist/Main.java
@@ -0,0 +1,12 @@
+package practicelinkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList(1);
+        ll.getHead();
+        ll.getLength();
+        ll.getTail();
+        ll.printAll();
+    }
+
+}
